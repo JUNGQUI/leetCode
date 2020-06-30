d2989e8e8c61c6c961ee00f883b83bebd5cf4cc0
jklee <jklee@goodsflow.com>
2020-06-30 11:59:50 +0900

    git init, TwoSum 1, 2 and ReverseInteger solved.

---
+++ src/main/java/com/jk/leetcode/reverseInteger/ReverseInteger.java
@@ -0,0 +1,26 @@
+package com.jk.leetcode.reverseInteger;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        if(x == 0 || x == Integer.MIN_VALUE || x == Integer.MAX_VALUE) return 0;
+
+        StringBuilder result = new StringBuilder();
+        char[] temp = String.valueOf(x).toCharArray();
+        int endFlag = -1;
+
+        if (x < 0) {
+            result.append("-");
+            endFlag++;
+        }
+
+        for (int i = temp.length-1; i > endFlag; i--) {
+            result.append(temp[i]);
+        }
+
+        try {
+            return Integer.parseInt(result.toString());
+        } catch (Exception ex) {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jk.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int[] numbers = new int[] {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = new int[]{};
+
+        System.out.println("J Tag");
+    }
+}
