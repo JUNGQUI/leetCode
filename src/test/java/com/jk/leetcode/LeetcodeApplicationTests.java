a84f3739862940895fbcf70eca0313a48e783ac0
jklee <jklee@goodsflow.com>
2020-07-17 17:43:01 +0900

    commit test, contribution

--- src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
@@ -13,24 +13,30 @@ class LeetcodeApplicationTests {
 
     @Test
     void contextLoads() {
-        int[] nums = new int[] {8, 1, 2, 2, 3};
+        int[] nums = new int[] {1, 1, 2, 3};
 
-        int[] count = new int[101];
-        int[] res = new int[nums.length];
+//        int[] count = new int[101];
+//        int[] res = new int[nums.length];
+//
+//        for (int i =0; i < nums.length; i++) {
+//            count[nums[i]]++;
+//        }
+//
+//        for (int i = 1 ; i <= 100; i++) {
+//            count[i] += count[i-1];
+//        }
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == 0)
+//                res[i] = 0;
+//            else
+//                res[i] = count[nums[i] - 1];
+//        }
 
-        for (int i =0; i < nums.length; i++) {
-            count[nums[i]]++;
-        }
+        
 
-        for (int i = 1 ; i <= 100; i++) {
-            count[i] += count[i-1];
-        }
+        for (int i = 0; i < nums.length/2; i++) {
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                res[i] = 0;
-            else
-                res[i] = count[nums[i] - 1];
         }
 
         System.out.println("J Tag");
