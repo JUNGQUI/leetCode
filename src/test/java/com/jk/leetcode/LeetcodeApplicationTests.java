c5e348e19907e6366b79cbaf47db5cfebe3df5ea
JUNGQUI <39726558+JUNGQUI@users.noreply.github.com>
2020-07-22 00:56:54 +0900

    Group The People Given The Group Size They Belog to solved. 33%

--- src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @SpringBootTest
@@ -12,16 +14,32 @@ class LeetcodeApplicationTests {
 
     @Test
     void contextLoads() {
-        int[] groupSizes = new int[] {2, 1, 3, 3, 3, 2};
+//        int[] groupSizes = new int[] {2, 1, 3, 3, 3, 2};
+        int[] groupSizes = new int[] {3, 3, 3, 3, 1, 3, 3};
 
         List<List<Integer>> results = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
-        List<Integer> contains = new ArrayList<>();
+        for (int i = 0; i < groupSizes.length; i++) {
+            if (map.containsKey(groupSizes[i])) {
+                List<Integer> temp = map.get(groupSizes[i]);
 
-        for (int i : groupSizes) {
-            if (!contains.contains(i)) {
-                contains.add(i);
+                if (temp.size() == groupSizes[i]) {
+                    results.add(temp);
+                    temp = new ArrayList<>();
+                }
+
+                temp.add(i);
+                map.put(groupSizes[i], temp);
+            } else {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(i);
+                map.put(groupSizes[i], temp);
             }
+        }
+
+        for (int i : map.keySet()) {
+            results.add(map.get(i));
         }
 
         System.out.println("J Tag");
