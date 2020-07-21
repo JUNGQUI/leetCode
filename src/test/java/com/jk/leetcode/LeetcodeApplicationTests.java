eceb49770af2a04b52bf09f02582567b7276fa61
jklee <ljk2518@gmail.com>
2020-07-21 17:29:13 +0900

    for pull, commit

--- src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
@@ -3,12 +3,18 @@ package com.jk.leetcode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class LeetcodeApplicationTests {
 
     @Test
     void contextLoads() {
-        int[] nums = new int[] {1, 1, 2, 3};
+        int[] groupSizes = new int[] {2, 1, 3, 3, 3, 2};
+
+        List results = new ArrayList<>();
+
 
 
         System.out.println("J Tag");
