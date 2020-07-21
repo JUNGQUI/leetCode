28d77fdcec356b0eb6481bb7f50100999433035b
jklee <ljk2518@gmail.com>
2020-07-21 17:20:37 +0900

    leetcode test init

--- src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
@@ -3,35 +3,12 @@ package com.jk.leetcode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
 @SpringBootTest
 class LeetcodeApplicationTests {
 
     @Test
     void contextLoads() {
         int[] nums = new int[] {1, 1, 2, 3};
-
-//        int[] count = new int[101];
-//        int[] res = new int[nums.length];
-//
-//        for (int i =0; i < nums.length; i++) {
-//            count[nums[i]]++;
-//        }
-//
-//        for (int i = 1 ; i <= 100; i++) {
-//            count[i] += count[i-1];
-//        }
-//
-//        for (int i = 0; i < nums.length; i++) {
-//            if (nums[i] == 0)
-//                res[i] = 0;
-//            else
-//                res[i] = count[nums[i] - 1];
-//        }
 
 
         System.out.println("J Tag");
