fbaea0ef3f1d245ae53f11d9ef55146565d952de
jklee <ljk2518@gmail.com>
2020-07-22 10:13:03 +0900

    Find a Corresponding Node Of a BinaryTree In a Clone Of That Tree solved

---
+++ src/main/java/com/jk/leetcode/treenode/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree.java
@@ -0,0 +1,31 @@
+package com.jk.leetcode.treenode;
+
+public class FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree {
+
+    class Solution {
+        public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+            if (original == null || original.val == target.val) {
+                return cloned;
+            }
+
+            TreeNode result = null;
+
+            if (original.left != null) {
+                result = getTargetCopy(original.left, cloned.left, target);
+            }
+
+            if (result != null) {
+                return result;
+            }
+
+            return getTargetCopy(original.right, cloned.right, target);
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
--- src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
@@ -12,18 +12,48 @@ class LeetcodeApplicationTests {
 
     @Test
     void contextLoads() {
-        int[] groupSizes = new int[] {2, 1, 3, 3, 3, 2};
 
-        List<List<Integer>> results = new ArrayList<>();
+        TreeNode original = new TreeNode(7);
+        original.left = new TreeNode(4);
+        original.right = new TreeNode(3);
+        original.right.left = new TreeNode(6);
+        original.right.right = new TreeNode(19);
 
-        List<Integer> contains = new ArrayList<>();
+        TreeNode cloned = new TreeNode(7);
+        cloned.left = new TreeNode(4);
+        cloned.right = new TreeNode(3);
+        cloned.right.left = new TreeNode(6);
+        cloned.right.right = new TreeNode(19);
 
-        for (int i : groupSizes) {
-            if (!contains.contains(i)) {
-                contains.add(i);
-            }
-        }
+        TreeNode target = new TreeNode(3);
+
+        TreeNode result = getTargetCopy(original, cloned, target);
 
         System.out.println("J Tag");
     }
+
+    private TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null || original.val == target.val) {
+            return cloned;
+        }
+
+        TreeNode result = null;
+
+        if (original.left != null) {
+            result = getTargetCopy(original.left, cloned.left, target);
+        }
+
+        if (result != null) {
+            return result;
+        }
+
+        return getTargetCopy(original.right, cloned.right, target);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 }
