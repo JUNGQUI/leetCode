3685ff2c3c6aae43d4ed80b3c8d8aea6e979b541
JUNGQUI <39726558+JUNGQUI@users.noreply.github.com>
2020-08-05 21:38:48 +0900

    Destination city solved, test

--- src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/jk/leetcode/LeetcodeApplicationTests.java
@@ -14,7 +14,35 @@ class LeetcodeApplicationTests {
 
     @Test
     void contextLoads() {
+        List<List<String>> paths = new ArrayList<>();
+        List<String> tempPath = new ArrayList<>();
+        tempPath.add("B");
+        tempPath.add("C");
+        paths.add(tempPath);
 
+        tempPath = new ArrayList<>();
+        tempPath.add("D");
+        tempPath.add("B");
+        paths.add(tempPath);
+
+        tempPath = new ArrayList<>();
+        tempPath.add("C");
+        tempPath.add("A");
+        paths.add(tempPath);
+
+        HashMap<String, String> result = new HashMap<>();
+
+        for (List<String> path : paths) {
+            if (!result.containsKey(path.get(1))) {
+                result.put(path.get(1), path.get(1));
+            }
+        }
+
+        for (List<String> path : paths) {
+            result.remove(path.get(0));
+        }
+
+//        return result.entrySet().iterator().hasNext() ? result.entrySet().iterator().next().getValue() : null;
 
         System.out.println("J Tag");
     }
