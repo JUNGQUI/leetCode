27bcf3d386f19ad58987a825576c5642ea320cbb
jklee <ljk2518@gmail.com>
2020-08-18 15:37:08 +0900

    - 문제 가지수가 많아져서 project 가 커져 별도 notepad class 만듬

---
+++ src/test/java/com/jk/leetcode/LeetCodeNotepad.java
@@ -0,0 +1,10 @@
+package com.jk.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class LeetCodeNotepad {
+    @Test
+    public void test() {
+        System.out.println("J Tag");
+    }
+}
