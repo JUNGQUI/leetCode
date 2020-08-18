8147cfcaab8d08f279b2d8287579599a455a524b
jklee <ljk2518@gmail.com>
2020-08-18 17:54:46 +0900

    - Reveal Cards In Increasing Order solving.

--- src/test/java/com/jk/leetcode/LeetCodeNotepad.java
+++ src/test/java/com/jk/leetcode/LeetCodeNotepad.java
@@ -5,6 +5,72 @@ import org.junit.jupiter.api.Test;
 public class LeetCodeNotepad {
     @Test
     public void test() {
+        int[] deck = new int[] {
+            2, 13, 3, 11, 5, 17, 7
+        };
+
+        int[] results = new int[deck.length];
+
+
+
         System.out.println("J Tag");
+    }
+
+    class CustomStack {
+
+        private int[] customStack;
+        private int currentIdx;
+
+        public CustomStack(int maxSize) {
+            customStack = new int[maxSize];
+            currentIdx = -1;
+        }
+
+        public void push(int x) {
+            if (currentIdx < customStack.length - 1) {
+                currentIdx++;
+                customStack[currentIdx] = x;
+            }
+        }
+
+        public int pop() {
+            int result = -1;
+
+            if (currentIdx != -1) {
+                result = customStack[currentIdx];
+                customStack[currentIdx] = 0;
+                currentIdx--;
+            }
+
+            return result;
+        }
+
+        public void increment(int k, int val) {
+            k = k > customStack.length ? customStack.length : k;
+            for (int i = 0; i < k; i++) {
+                customStack[i]+=val;
+            }
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        LeetcodeApplicationTests.TreeNode left;
+        LeetcodeApplicationTests.TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, LeetcodeApplicationTests.TreeNode left, LeetcodeApplicationTests.TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        LeetcodeApplicationTests.ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, LeetcodeApplicationTests.ListNode next) { this.val = val; this.next = next; }
     }
 }
