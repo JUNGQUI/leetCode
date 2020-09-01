ae819d5b900d2011d4252a5d64e281c88402ee4c
JUNGQUI <ljk2518@gmail.com>
2020-09-01 21:01:36 +0900

    static method, variable test.

--- src/test/java/com/jk/leetcode/LeetCodeNotepad.java
+++ src/test/java/com/jk/leetcode/LeetCodeNotepad.java
@@ -10,43 +10,18 @@ public class LeetCodeNotepad {
 
     @Test
     public void test() {
-        int[][] A = new int[][] {
-                {0, 0, 1, 1},
-                {1, 0, 1, 0},
-                {1, 1, 0, 0}
-        };
-
-        int row = A.length, column = A[0].length;
-        int result = 0;
-
-        for (int i = 0; i < row; i++) {
-            if (A[i][0] == 0) {
-                for (int j = 0; j < column; j++) {
-                    A[i][j] = A[i][j]^1;
-                }
-            }
-        }
-
-        for (int i = 0; i < column; i++) {
-            int numberOf1 = 0;
-            for (int[] ints : A) {
-                numberOf1 += ints[i];
-            }
-
-            if (numberOf1 <= row/2) {
-                for (int j = 0; j < row; j++) {
-                    A[j][i] = A[j][i]^1;
-                }
-            }
-        }
-
-        for (int i = 0; i < column; i++) {
-            for (int[] ints : A) {
-                result += ints[i] * Math.pow(2, column - 1 - i);
-            }
-        }
-
+        StaticClass staticClass1 = new StaticClass();
+        StaticClass staticClass2 = new StaticClass();
         System.out.println("J Tag");
+    }
+
+    static class StaticClass {
+        private static int count;
+
+        public StaticClass() {
+            count ++;
+            System.out.println("좋아요 갯수 : " + count);
+        }
     }
 
     class CustomStack {
