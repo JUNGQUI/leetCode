031117c368695246c9d01fde110b97c9ab89ec6f
JUNGQUI <ljk2518@gmail.com>
2020-09-15 18:31:10 +0900

    어렵다..

--- src/test/java/com/jk/leetcode/LeetCodeNotepad.java
+++ src/test/java/com/jk/leetcode/LeetCodeNotepad.java
@@ -10,50 +10,7 @@ public class LeetCodeNotepad {
 
     @Test
     public void test() {
-        sortArrayByParity(new int[] {4, 3, 2, 1});
         System.out.println("J Tag");
-    }
-
-    public int[] sortArrayByParity(int[] A) {
-        int[] results = new int[A.length];
-        int evenIndex = 0, oddIndex = A.length-1;
-
-        for (int j : A) {
-            if (j % 2 == 0) {
-                results[evenIndex] = j;
-                evenIndex++;
-            } else {
-                results[oddIndex] = j;
-                oddIndex--;
-            }
-        }
-
-        return results;
-    }
-
-    public int[] jkSortArrayByParity(int[] A) {
-        int[] results = new int[A.length];
-
-        List<Integer> tempResults = new ArrayList<>();
-        List<Integer> even = new ArrayList<>();
-        List<Integer> odd = new ArrayList<>();
-
-        for (int element : A) {
-            if (element % 2 == 0) {
-                even.add(element);
-            } else {
-                odd.add(element);
-            }
-        }
-
-        tempResults.addAll(even);
-        tempResults.addAll(odd);
-
-        for (int i = 0; i < A.length; i++) {
-            results[i] = tempResults.get(i);
-        }
-
-        return results;
     }
 
     class Node {
