042709b15ae31b6b911b885959046966802d6f30
[백엔드 개발자]이정기 <ljk2518@gmail.com>
2020-10-05 22:12:53 +0900

    ParkingSystem solved

---
+++ src/main/java/com/jk/leetcode/simple/parking/package-info.java
@@ -0,0 +1,14 @@
+package com.jk.leetcode.simple.parking;
+
+/*
+public ParkingSystem(int big, int medium, int small)
+parking system 을 만든다. parameter 순서대로 대형차 / 중형차 / 소형차 가 설 수 있는 주차장 개수이다.
+
+public boolean addCar(int carType)
+주어지는 int 가 1일 경우 대형차, 2일 경우 중형차, 3일 경우 소형차 이다.
+
+주차장은 주차만 할뿐 출차하지 않는다. 이럴 경우 두 method 를 완성하라.
+
+첫 method 는 생성자이니 구현하면 된다.
+이후 static 을 통해 값을 제어하면 완성
+ */
