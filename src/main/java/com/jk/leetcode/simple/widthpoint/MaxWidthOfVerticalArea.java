de33b18b17162e1a31546023443558cb4296626e
[백엔드 개발자]이정기 <ljk2518@gmail.com>
2020-12-09 21:07:07 +0900

    max width of vertical area init
    - TDD
    - 객체지향적 풀기
    - README 로 일관되게 (패키지 인포 사용 X)

---
+++ src/main/java/com/jk/leetcode/simple/widthpoint/MaxWidthOfVerticalArea.java
@@ -0,0 +1,7 @@
+package com.jk.leetcode.simple.widthpoint;
+
+public class MaxWidthOfVerticalArea {
+	public int maxWidthOfVerticalArea(int[][] points) {
+		return 0;
+	}
+}
---
+++ src/test/java/com/jk/leetcode/simple/widthpoint/MaxWidthOfVerticalAreaTest.java
@@ -0,0 +1,19 @@
+package com.jk.leetcode.simple.widthpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxWidthOfVerticalAreaTest {
+
+	@Test
+	void maxWidthOfVerticalArea() {
+		int[][] points = new int[][] {
+				{}
+		};
+	}
+
+	private int maxWidthOfVerticalArea(int[][] points) {
+		return 0;
+	}
+}
