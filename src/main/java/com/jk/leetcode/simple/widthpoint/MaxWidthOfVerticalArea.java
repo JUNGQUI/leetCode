63a4d2b77790721a77e405bd77761e2193f2040a
[백엔드 개발자]이정기 <ljk2518@gmail.com>
2020-12-09 21:51:41 +0900

    max width of vertical area solved
    - TDD
    - 객체지향적 풀기
    - README 로 일관되게 (패키지 인포 사용 X)

--- src/main/java/com/jk/leetcode/simple/widthpoint/MaxWidthOfVerticalArea.java
+++ src/main/java/com/jk/leetcode/simple/widthpoint/MaxWidthOfVerticalArea.java
@@ -1,7 +1,18 @@
 package com.jk.leetcode.simple.widthpoint;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MaxWidthOfVerticalArea {
 	public int maxWidthOfVerticalArea(int[][] points) {
-		return 0;
+		Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
+
+		int result = 0;
+
+		for (int i = 0; i < points.length - 1; i++) {
+			result = Math.max((points[i+1][0] - points[i][0]), result);
+		}
+
+		return result;
 	}
 }
--- src/test/java/com/jk/leetcode/simple/widthpoint/MaxWidthOfVerticalAreaTest.java
+++ src/test/java/com/jk/leetcode/simple/widthpoint/MaxWidthOfVerticalAreaTest.java
@@ -1,5 +1,8 @@
 package com.jk.leetcode.simple.widthpoint;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,12 +11,27 @@ class MaxWidthOfVerticalAreaTest {
 
 	@Test
 	void maxWidthOfVerticalArea() {
-		int[][] points = new int[][] {
-				{}
+		int[][] points = new int[][]{
+				{3,1},
+				{9,0},
+				{1,0},
+				{1,4},
+				{5,3},
+				{8,8}
 		};
+
+		Assertions.assertEquals(3, maxWidthOfVerticalArea(points));
 	}
 
 	private int maxWidthOfVerticalArea(int[][] points) {
-		return 0;
+		Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
+
+		int result = 0;
+
+		for (int i = 0; i < points.length - 1; i++) {
+			result = Math.max((points[i+1][0] - points[i][0]), result);
+		}
+
+		return result;
 	}
 }
