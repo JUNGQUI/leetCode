96055911740871d645d7d7e80abc7e2648f08b63
[백엔드 개발자]이정기 <ljk2518@gmail.com>
2020-12-11 19:31:59 +0900

    MergeInBetween solving
    - TDD
    - 객체지향적 풀기
    - README 로 일관되게 (패키지 인포 사용 X)
    - README 에 공략법 추가

---
+++ src/main/java/com/jk/leetcode/node/merge/MergeInBetween.java
@@ -0,0 +1,9 @@
+package com.jk.leetcode.node.merge;
+
+import com.jk.leetcode.node.ListNode;
+
+public class MergeInBetween {
+	public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+
+	}
+}
---
+++ src/test/java/com/jk/leetcode/node/merge/MergeInBetweenTest.java
@@ -0,0 +1,15 @@
+package com.jk.leetcode.node.merge;
+
+import com.jk.leetcode.LeetCodeNotepad.ListNode;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeInBetweenTest {
+	@Test
+	void mergeInBetweenTest() {
+		ListNode listNode1 = new ListNode();
+		ListNode listNode2 = new ListNode();
+		int a = 3, b = 4;
+	}
+}
