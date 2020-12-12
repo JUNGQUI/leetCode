ca3f790b29238da3fd2337da9dd5f4df0e679242
[백엔드 개발자]이정기 <ljk2518@gmail.com>
2020-12-12 13:37:39 +0900

    MergeInBetween solving
    - TDD
    - 객체지향적 풀기
    - README 로 일관되게 (패키지 인포 사용 X)
    - README 에 공략법 추가
    - recursive 로 할지, 단순 for 로 할지 고민중

--- src/main/java/com/jk/leetcode/node/merge/MergeInBetween.java
+++ src/main/java/com/jk/leetcode/node/merge/MergeInBetween.java
@@ -4,6 +4,6 @@ import com.jk.leetcode.node.ListNode;
 
 public class MergeInBetween {
 	public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-
+		return null;
 	}
 }
--- src/test/java/com/jk/leetcode/node/merge/MergeInBetweenTest.java
+++ src/test/java/com/jk/leetcode/node/merge/MergeInBetweenTest.java
@@ -8,8 +8,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MergeInBetweenTest {
 	@Test
 	void mergeInBetweenTest() {
-		ListNode listNode1 = new ListNode();
-		ListNode listNode2 = new ListNode();
-		int a = 3, b = 4;
+
+		ListNode listNode1 = new ListNode(
+				0, new ListNode(1,
+				new ListNode(2, new ListNode(3,
+						new ListNode(4, new ListNode(5)))
+				)
+		));
+
+		ListNode listNode2 = new ListNode(1000000, new ListNode(1000001, new ListNode(1000002)));
+
+		ListNode listNode3 = new ListNode(
+				0, new ListNode(1,
+				new ListNode(2, new ListNode(1000000,
+						new ListNode(1000001, new ListNode(1000002,
+								new ListNode(5))
+						)
+				))
+		));
+
+		int a = 3, b = 4, currentIdx = 0;
+
+		ListNode result = new ListNode(listNode1.val, listNode1.next);
+
+		while (listNode1.next != null) {
+			if (currentIdx == a) {
+				result.next = new ListNode(listNode2.val, listNode2.next);
+			}
+
+			if (currentIdx == b) {
+				result.next = new ListNode(listNode1.val, listNode1.next);
+				break;
+			}
+
+			currentIdx++;
+			listNode1 = listNode1.next;
+			result = result.next != null ? result.next : null;
+		}
 	}
 }
